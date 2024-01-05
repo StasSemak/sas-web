7f210225336699bd0b8e2cada5e1ddfa7477285a
StasSemak <semaks051@gmail.com>
2024-01-05 19:04:36 +0200

    feat: automatic footer year

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,7 +6,7 @@ export const Footer = async () => {
     return(
         <footer className="h-20 bg-blue-950 border-t border-zinc-300">
             <div className="max-w-[1440px] mx-auto h-full px-4 md:px-[60px] flex flex-row items-center text-zinc-100 justify-between py-1">
-                <p>Copyright &copy; 2023</p>
+                <p>&copy; {new Date(Date.now()).getFullYear()}</p>
                 <p className="hidden md:block max-w-[400px] text-xs text-blue-400">
                     Додаток знаходиться у стадії розробки. Певний функціонал може бути обмежений або працювати некоректно.
                     {' '}
